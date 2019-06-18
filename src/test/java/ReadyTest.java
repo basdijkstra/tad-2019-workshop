f0774bbb526016ee400f2ad26af1c79282d7b9b0
Bas Dijkstra <bas@ontestautomation.com>
2019-06-18 11:29:02 +0200

    Added check to see if everything's set up properly

--- src/test/java/ReadyTest.java
+++ src/test/java/ReadyTest.java
@@ -1,34 +1,21 @@
-package answers;
-
 import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import org.junit.*;
 
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
 import static io.restassured.RestAssured.given;
 
-public class EncapsulationTest {
+public class ReadyTest {
 
     @Rule
     public WireMockRule wireMockRule = new WireMockRule(options().port(9876));
 
-    private Car car;
-
-    @Before
-    public void createCar() {
-
-        car = new Car();
-    }
-
     @Test
-    public void testCarSerialization() {
+    public void areYouReadyForTheWorkshopTest() {
 
         // Act
         given().
-            log().body().
-        and().
-            body(car).
         when().
-            post("http://localhost:9876/car").
+            get("http://localhost:9876/areyouready").
         then().
             assertThat().
             statusCode(200);
